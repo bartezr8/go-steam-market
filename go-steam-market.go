d57595a1d6429adcdc472e61eb01a731856c9e02
Gacnt <gacnt@shaw.ca>
2015-08-28 22:31:07 -0600

    Added Pricing and AssetClassInfo

--- go-steam-market.go
+++ go-steam-market.go
@@ -3,6 +3,7 @@ package gosm
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/url"
 	"time"
@@ -26,6 +27,62 @@ type AssetPrice struct {
 	LowPrice string `json:"lowest_price"`
 	Volume   string `json:"volume"`
 	MedPrice string `json:"median_price"`
+}
+
+type AssetIntermediate struct {
+	Result map[string]json.RawMessage `json:"result"`
+}
+
+type AssetInfo struct {
+	Assets  map[string]Asset `json:"assets"`
+	Success bool             `json:"success"`
+	Error   string           `json:"error"`
+}
+
+type Asset struct {
+	IconUrl           string                 `json:"icon_url,omitempty"`
+	IconUrlLarge      string                 `json:"icon_url_large,omitempty"`
+	IconDragUrl       string                 `json:"icon_drag_url,omitempty"`
+	Name              string                 `json:"name,omitempty"`
+	MarketHashName    string                 `json:"market_hash_name,omitempty"`
+	MarketName        string                 `json:"market_name,omitempty"`
+	NameColor         string                 `json:"name_color,omitempty"`
+	BGColor           string                 `json:"background_color,omitempty"`
+	Type              string                 `json:"type,omitempty"`
+	Tradable          string                 `json:"tradable,omitempty"`
+	Marketable        string                 `json:"marketable,omitempty"`
+	Commodity         string                 `json:"commodity,omitempty"`
+	TradeRestrict     string                 `json:"market_tradeable_restriction,omitempty"`
+	FraudWarnings     string                 `json:"fraudwarnings,omitempty"`
+	Descriptions      map[string]Description `json:"descriptions,omitempty"`
+	OwnerDescriptions string                 `json:"owner_descriptions,omitempty"`
+	Tags              map[string]Tag         `json:"tags,omitempty"`
+	ClassId           string                 `json:"classid,omitempty"`
+}
+
+type Description struct {
+	Type    string `json:"type"`
+	Value   string `json:"value"`
+	Color   string `json:"color,omitempty"`
+	AppData string `json:"appdata"`
+}
+
+type Tag struct {
+	InternalName string `json:"internal_name"`
+	Name         string `json:"name"`
+	Category     string `json:"category"`
+	Color        string `json:"color,omitempty"`
+	CategoryName string `json:"category_name"`
+}
+
+func (a *Asset) GetPrice() *AssetPrice {
+	resp, err := GetSinglePricePrepped(a.MarketHashName)
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+
 }
 
 func MarketHash(stattrak bool, wep, skin, wear string) string {
@@ -87,5 +144,95 @@ func GetSinglePrice(stattrak bool, wep, skin, wear, item string) *AssetPrice {
 	}
 
 	return &jsonResp
+
+}
+
+func GetSinglePricePrepped(name string) (*AssetPrice, error) {
+	var Url *url.URL
+
+	Url, err := url.Parse(baseUrl + "/market/priceoverview")
+	if err != nil {
+		panic(err)
+	}
+
+	parameters := url.Values{}
+	parameters.Add("currency", Currency)
+	parameters.Add("appid", AppId)
+	parameters.Add("market_hash_name", name)
+	Url.RawQuery = parameters.Encode()
+
+	client := http.Client{Timeout: time.Duration(60) * time.Second}
+	req, _ := http.NewRequest("GET", Url.String(), nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var jsonResp AssetPrice
+
+	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &jsonResp, nil
+
+}
+
+func GetAssetInfo(key, appid, class_count, classid, instanceid string) (*AssetInfo, error) {
+	var Url *url.URL
+	Url, err := url.Parse("http://api.steampowered.com/ISteamEconomy/GetAssetClassInfo/v0001/")
+
+	parameters := url.Values{}
+	parameters.Add("class_count", class_count)
+	parameters.Add("classid0", classid)
+	parameters.Add("instanceid0", instanceid)
+	parameters.Add("appid", appid)
+	parameters.Add("key", key)
+	Url.RawQuery = parameters.Encode()
+
+	client := http.Client{Timeout: time.Duration(60) * time.Second}
+	req, _ := http.NewRequest("GET", Url.String(), nil)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var intermediate AssetIntermediate
+
+	json.NewDecoder(resp.Body).Decode(&intermediate)
+
+	var ai AssetInfo
+
+	for k, v := range intermediate.Result {
+		r := bytes.NewReader(v)
+		d := json.NewDecoder(r)
+		var err error
+		if k == "error" {
+			err = d.Decode(&ai.Error)
+		} else if k == "success" {
+			err = d.Decode(&ai.Success)
+		} else {
+			var a Asset
+			err = d.Decode(&a)
+			if err == nil {
+				if ai.Assets == nil {
+					ai.Assets = map[string]Asset{}
+				}
+				ai.Assets[k] = a
+			}
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return &ai, nil
 
 }
